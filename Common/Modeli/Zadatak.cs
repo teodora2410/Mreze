7292ed5fe47d648c377135a34b91477be5eb911c
teodora2410 <tceremidzic01@gmail.com>
2026-01-22 21:39:22 +0100

    klasa zadatak

---
+++ Common/Modeli/Zadatak.cs
@@ -0,0 +1,37 @@
+﻿using Common.Enumeracije;
+using System;
+namespace Common
+{
+    [Serializable]
+    public class Zadatak
+    {
+        public TipZadatka Tip { get; set; }
+        public int ApartmanId { get; set; }
+
+        public Zadatak() { }
+
+        public Zadatak(TipZadatka tip, int apartmanId)
+        {
+            Tip = tip;
+            ApartmanId = apartmanId;
+        }
+    }
+
+    [Serializable]
+    public class PotvrdaOUradjenomZadatku
+    {
+        public int ApartmanId { get; set; }
+        public TipZadatka Tip { get; set; }
+        public bool Uspesno { get; set; }
+
+        public PotvrdaOUradjenomZadatku() { }
+
+        public PotvrdaOUradjenomZadatku(int apartmanId, TipZadatka tip, bool uspesno)
+        {
+            ApartmanId = apartmanId;
+            Tip = tip;
+            Uspesno = uspesno;
+        }
+    }
+}
+
