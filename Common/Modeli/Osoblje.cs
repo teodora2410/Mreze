dab144c342fec574f92944b334b457d771e92db1
tamarabuha <tamara.buha@gmail.com>
2026-01-22 21:48:20 +0100

    klasa osoblje

---
+++ Common/Modeli/Osoblje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Common
+{
+    [Serializable]
+    public class Osoblje
+    {
+        public int Id { get; set; } = 0;
+        public string Ime { get; set; } = "";
+        public string Prezime { get; set; } = "";
+        public string Pol { get; set; } = "";
+        public string Funkcija { get; set; } = "";
+
+        public Osoblje() { }
+
+        public Osoblje(int id, string ime, string prezime, string pol, string funkcija)
+        {
+            Id = id;
+            Ime = ime;
+            Prezime = prezime;
+            Pol = pol;
+            Funkcija = funkcija;
+        }
+    }
+}
