9ce3a58931f0bc7e4f337940322b7e40ab877ecc
tamarabuha <tamara.buha@gmail.com>
2026-01-22 21:49:03 +0100

    forma zahteva za kreiranje rezervacije

---
+++ Common/Modeli/ZahtevZaRezervacijuApartmana.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Common.Modeli
+{
+    [Serializable]
+    public class ZahtevZaRezervacijuApartmana
+    {
+        public int BrojGostiju { get; set; } = 0;
+        public int Klasa { get; set; } = 0;
+        public int BrojNoci { get; set; } = 0;
+
+        public ZahtevZaRezervacijuApartmana() { }
+
+        public ZahtevZaRezervacijuApartmana(int brojGostiju, int klasa, int brojNoci)
+        {
+            BrojGostiju = brojGostiju;
+            Klasa = klasa;
+            BrojNoci = brojNoci;
+        }
+    }
+}
