7e3111a31457d465d09ec59343577bc891d8418c
tamarabuha <tamara.buha@gmail.com>
2026-01-22 21:48:44 +0100

    informacije o rezervaciji

---
+++ Common/Modeli/RezervacijaInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.Modeli
+{
+    [Serializable]
+    public class RezervacijaInfo
+    {
+        public Apartman Apartman { get; set; }
+        public int ApartmanId { get; set; }
+        public List<Gost> Gosti { get; set; } = new List<Gost>();
+        public List<string> Narudzbine { get; set; } = new List<string>();
+        public int BrojNoci { get; set; }
+
+        public RezervacijaInfo() { }
+
+        public RezervacijaInfo(Apartman apartman, int apartmanId, List<Gost> gosti, List<string> narudzbine, int brojNoci)
+        {
+            Apartman = apartman;
+            ApartmanId = apartmanId;
+            Gosti = gosti;
+            Narudzbine = narudzbine;
+            BrojNoci = brojNoci;
+        }
+    }
+}
