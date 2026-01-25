598654899fa0fcc192062d42f9c7cad135394d8f
teodora2410 <tceremidzic01@gmail.com>
2026-01-25 10:54:55 +0100

    priprema za rezervisanje

---
+++ Server Aplikacija/Operacije/Rezervisanje.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using Common.Enumeracije;
+using Common.Modeli;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace Server_Aplikacija.Operacije
+{
+    public class Rezervisanje
+    {
+        List<Apartman> apartmani;
+        Dictionary<Socket, RezervacijaInfo> aktivneRezervacije;
+
+        public Rezervisanje(List<Apartman> apartmani, Dictionary<Socket, RezervacijaInfo> aktivneRezervacije)
+        {
+            this.apartmani = apartmani;
+            this.aktivneRezervacije = aktivneRezervacije;
+        }
+
+      
+}
