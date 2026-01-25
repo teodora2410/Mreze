09ad9952f5559d1da98407faf9480b8cb8f1b1f4
teodora2410 <tceremidzic01@gmail.com>
2026-01-25 10:55:18 +0100

    prijem podataka o rezervaciji

--- Server Aplikacija/Operacije/Rezervisanje.cs
+++ Server Aplikacija/Operacije/Rezervisanje.cs
@@ -18,5 +18,25 @@ namespace Server_Aplikacija.Operacije
             this.aktivneRezervacije = aktivneRezervacije;
         }
 
-      
+        public void ObradaRezervacije(Socket klijent, object payload)
+        {
+            int brojGostiju = 0;
+            int klasa = 0;
+            int brojNoci = 1;
+
+            if (payload is ZahtevZaRezervacijuApartmana zahtev)
+            {
+                brojGostiju = zahtev.BrojGostiju;
+                klasa = zahtev.Klasa;
+                brojNoci = zahtev.BrojNoci > 0 ? zahtev.BrojNoci : 1;
+            }
+            else if (payload is int broj)
+            {
+                brojGostiju = broj;
+                klasa = 0;
+                brojNoci = 1;
+            }
+
+        }
+    }
 }
