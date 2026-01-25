66d9ec93744a5b9bf23b540519204a0201f2227c
teodora2410 <tceremidzic01@gmail.com>
2026-01-25 10:52:16 +0100

    serijalizacija podataka

---
+++ Common/Serializator.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Common
+{
+    public static class MemorySerializer
+    {
+        public static byte[] Serialize(object obj)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+      
+    }
+}
