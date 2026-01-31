c267f4da6dd7a702b1ae87f274404119090f26ef
tamarabuha <tamara.buha@gmail.com>
2026-01-31 04:46:03 +0100

    pokretanje klijenta

---
+++ Klijent Gost/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Klijent_Gost
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Dobrodosli u hotelski kompleks!");
+
+            Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                tcpSocket.Connect(new IPEndPoint(IPAddress.Loopback, 9000));
+                Console.WriteLine("Povezano sa serverom.");
+            }
+            catch
+            {
+                Console.WriteLine("Server nije dostupan!");
+                Console.ReadKey();
+                return;
+            }
+
+            tcpSocket.Close();
+            Console.WriteLine("\nPritisnite bilo koji taster za izlaz...");
+            Console.ReadKey();
+        }
+    }
+}
