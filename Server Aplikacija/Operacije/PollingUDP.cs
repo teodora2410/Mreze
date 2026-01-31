901b0e78c906b01a2f5c84cfa3e13f036d59cb84
tamarabuha <tamara.buha@gmail.com>
2026-01-31 04:47:43 +0100

    priprema servera za osoblje

---
+++ Server Aplikacija/Operacije/PollingUDP.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using Common.Enumeracije;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server_Aplikacija.Operacije
+{
+    public class PollingUDP
+    {
+        Socket udpSocket;
+        List<EndPoint> osobljeUDPKlijenti;
+        List<Apartman> apartmani;
+
+        public PollingUDP(Socket udpSocket, List<EndPoint> osobljeUDPKlijenti, List<Apartman> apartmani)
+        {
+            this.udpSocket = udpSocket;
+            this.osobljeUDPKlijenti = osobljeUDPKlijenti;
+            this.apartmani = apartmani;
+        }
+
+     
+    }
+}
