b500b60c539b4a954c3c7aa54a8895f392e58a09
teodora2410 <tceremidzic01@gmail.com>
2026-02-01 04:50:57 +0100

    alarmiranje

---
+++ Server Aplikacija/Operacije/Alarmiranje.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using Common.Enumeracije;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server_Aplikacija.Operacije
+{
+    public class Alarmiranje
+    {
+        List<Apartman> apartmani;
+        List<EndPoint> osobljeUDPKlijenti;
+        Socket udpSocket;
+
+        public Alarmiranje(List<Apartman> apartmani, List<EndPoint> osobljeUDPKlijenti, Socket udpSocket)
+        {
+            this.apartmani = apartmani;
+            this.osobljeUDPKlijenti = osobljeUDPKlijenti;
+            this.udpSocket = udpSocket;
+        }
+
+     
+    }
+}
