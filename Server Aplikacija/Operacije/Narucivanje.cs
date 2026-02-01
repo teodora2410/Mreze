5071d10ab78f34d7dc4b1a3c98ee8eee347b02b9
teodora2410 <tceremidzic01@gmail.com>
2026-02-01 04:52:17 +0100

    prosirenje narudzbine sa alarmima gosta

---
+++ Server Aplikacija/Operacije/Narucivanje.cs
@@ -0,0 +1,31 @@
+﻿using Common.Modeli;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace Server_Aplikacija.Operacije
+{
+    public class Narucivanje
+    {
+        Dictionary<Socket, RezervacijaInfo> aktivneRezervacije;
+
+        public Narucivanje(Dictionary<Socket, RezervacijaInfo> aktivneRezervacije)
+        {
+            this.aktivneRezervacije = aktivneRezervacije;
+        }
+
+        public void ObradaNarudzbine(Socket klijent, string tip)
+        {
+            if (!aktivneRezervacije.ContainsKey(klijent))
+            {
+                Console.WriteLine("[Greška] Klijent nema aktivnu rezervaciju");
+                return;
+            }
+
+            var info = aktivneRezervacije[klijent];
+            info.Narudzbine.Add(tip);
+
+            Console.WriteLine($"[Server] Narudzbina '{tip}' zabelezena za apartman {info.Apartman.BrojApartmana}");
+        }
+    }
+}
