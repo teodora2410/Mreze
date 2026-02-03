bf53605dc00f5d96e8d77c1814ada220a5fdddc6
teodora2410 <tceremidzic01@gmail.com>
2026-02-03 04:54:02 +0100

    klijent osoblje

---
+++ Klijent Osoblje/Program.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+using Common.Enumeracije;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Klijent_Osoblje
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+         
+        }
+    }
+}
